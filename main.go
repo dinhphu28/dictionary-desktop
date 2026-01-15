6cafe784874d04777bb9c8cdace76a84b4a6a290
Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>
2026-01-15 17:32:04 +0700

    Refactor frontend
    Signed-off-by: Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>

--- main.go
+++ main.go
@@ -21,13 +21,13 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "dictionary-desktop",
+		Title:  "Dictionary",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 245, G: 245, B: 247, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
